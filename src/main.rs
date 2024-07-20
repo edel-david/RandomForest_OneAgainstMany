629dce1269c67f04cdae5f0bd7281c4867166138
david <david.edel@gmx.de>
2024-07-20 04:28:13 +0200

    not working, always returning -1

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,121 @@
 #![allow(unused)]
+mod utils;
+//extern crate supa_cube_forest;
+//use crate::utils::*;
 use core::hash::Hash;
 use core::ops::Add;
+use std::cmp::Ordering;
+use std::fmt::{Debug, Error};
+use std::hash::Hasher;
+use std::ops::{Deref, Div, Mul, Neg, Rem, Sub};
+use std::str::FromStr;
+use std::{mem, usize};
 //use core::slice::SlicePattern;
-use ndarray::{prelude::*, ViewRepr};
-use ndarray_npy::read_npy;
-use num::{Float, FromPrimitive, NumCast};
+use conv::prelude::*;
+use ndarray::{prelude::*, ScalarOperand, ViewRepr};
+use ndarray_npy::{read_npy, ReadableElement};
+use num::{Float, FromPrimitive, NumCast, One, ToPrimitive, Zero};
 use rand::seq::SliceRandom;
 use std::collections::HashSet;
+use std::{collections::HashMap, hash};
 use std::{f64::INFINITY, marker::PhantomData};
-fn main() {
-    // load data and target
-    //const dim :Dim<[usize; 2]> = Dim([1797,64]);
-    let data: Array2<f64> = read_npy("src/digits_data.npy").expect("file is present and correct");
-    let target: Array1<i64> =
-        read_npy("src/digits_target.npy").expect("file is present and correct");
-
-    println!("{:?}", data.dim());
-    println!("{:?}", target.dim());
-}
-// T is the type of the prediction value
-//G is type of feature data Vec<>
-enum Node<T: Clone, G: Clone> {
+#[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
+pub struct Float64(f64);
+#[derive(Debug)]
+pub enum Node<'a, R: Clone + num::Zero, G: Clone> {
     SplitNode {
-        left: Option<Box<Node<T, G>>>,
-        right: Option<Box<Node<T, G>>>,
+        left: Option<Box<Node<'a, R, G>>>,
+        right: Option<Box<Node<'a, R, G>>>,
         split_index: usize,
         threashold: G,
     },
     LeafNode {
-        prediction: T,
+        prediction: R,
     },
+    UnknownNode {
+        features:&'a Array1<G>,
+        targets:&'a Array1<G>
+    }
 }
 
+
+
+fn main() {
+    type G = Float64;
+    type T = i64;
+    type R = Float64;
+    
+    let size = mem::size_of::<Node<R,G>>();
+    println!("Size of MyEnum: {} bytes", size);
+
+    
+    // let data: Array2<f64> = read_npy("src/digits_data.npy").expect("file is present and correct");
+    // let data = data.mapv(|x| Float64(x));
+    // let target: Array1<i64> =
+    //     read_npy("src/digits_target.npy").expect("file is present and correct");
+    
+    let data: Array2<f64> = read_npy("src/three_9_data.npy").expect("file is present and correct");
+    let data = array![[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]];
+    let data = data.mapv(|x| Float64(x as f64));
+    let target: Array1<i64> =
+    read_npy("src/three_9_target.npy").expect("file is present and correct");
+    dbg!(target.sum());
+    let target = array![-1,-1,1,1];
+
+    let mut tree: RegressionTree<G, R> = RegressionTree {
+        root: None,
+        n_min: 2,
+        d_try:2
+    };
+
+
+    tree.train(&data,&target,None);
+    dbg!(&tree);
+    println!("{:?}", data.dim());
+    println!("{:?}", target.dim());
+
+    let mut results = Vec::new();
+    for row in data.rows() {
+        results.push(tree.predict(&row.to_owned()))
+    }
+    dbg!(&results);
+
+}
+// T is the type of the response values
 //G is type of feature data Vec<>
-struct RegressionTree<T: Clone, G: Clone>
+// R is the predict return Type
+#[derive(Debug)]
+pub struct RegressionTree<'a,G: Clone, R>
 where
-    f64: From<G>,
-    G: From<f64> + FromPrimitive + Float + From<usize>,
-    T: std::convert::From<f64>,
+    G: From<f64> + FromPrimitive + Float, //From<usize>,
+    R: Clone + num::Zero,
 {
-    root: Option<Box<Node<T, G>>>,
+    root: Option<Box<Node<'a,R, G>>>,
     n_min: usize,
+    d_try:usize
     //input_data: PhantomData<G>,
 }
 
-impl<T: Clone, G: Clone + std::cmp::PartialOrd> RegressionTree<T, G>
+impl<'a,G: Clone + std::cmp::PartialOrd, R> RegressionTree<'a,G, R>
 where
-    f64: From<G>,
     G: Add
+        + Debug
+        + ValueFrom<usize>
         + From<f64>
         + Hash
         + FromPrimitive
         + Float
-        + From<usize>
-        + From<T>
+        + From<u8>
+        + From<R>
         + Ord
         + ndarray::ScalarOperand
         + std::ops::Add<Output = G>
         + core::ops::Add
         + Copy,
-
-    T: std::convert::From<f64>,
+    R: Clone + num::Zero + From<f64>+From<usize> + std::ops::Div<Output = R>,
+    //T: std::convert::From<f64> + num::Zero,
 {
-    fn predict(&self, x: Vec<G>) -> T {
+    pub fn predict(&self, x: &Array1<G>) -> R {
         let mut node = &(self.root);
         for _i in 0..100 {
             match node.as_ref() {
@@ -82,9 +135,11 @@ where
                             node = right
                         }
                     }
+                    Node::UnknownNode { features, targets }
+                => {return 0_f64.into()}
                 },
                 None => {
-                    return T::from(0_f64); // if not trained yet, return 0 always
+                    return R::from(0_f64); // if not trained yet, return 0 always
                                            // if trained but still reached => bug
                                            // consider logging here, as it does not make sense to use predict before training
                 }
@@ -94,7 +149,7 @@ where
         todo!("predict followed split node for over 100 splits! not possible?");
     }
     /// j is feature index and t is threashold
-    fn compute_loss_for_split(
+    fn compute_loss_for_split<T: Clone + Into<G>>(
         &self,
         features: &Array2<G>,
         responses: &Array1<T>,
@@ -111,18 +166,18 @@ where
             return <G as From<f64>>::from(INFINITY);
         }
         let left = responses
-            .select(Axis(1), left_indices.as_slice().expect("idk if this works"))
-            .mapv(|x| <G as From<T>>::from(x));
+            .select(Axis(0), left_indices.as_slice().expect("idk if this works"))
+            .mapv(|x| x.into());
 
         let right = responses
             .select(
-                Axis(1),
+                Axis(0),
                 right_indices.as_slice().expect("idk if this works"),
             )
-            .mapv(|x| <G as From<T>>::from(x));
+            .mapv(|x| x.into());
         let zero = <G as From<f64>>::from(0_f64);
-        let loss = left.var(zero) * <G as From<usize>>::from(amount_left)
-            + right.var(zero) * <G as From<usize>>::from(amount_right);
+        let loss = left.var(zero) * G::value_from(amount_left).expect("msg")// into G
+            + right.var(zero) * G::value_from(amount_right).expect("msg");
         return loss;
     }
 
@@ -133,7 +188,12 @@ where
         myvec.shuffle(&mut rng);
         myvec[..d_try].to_vec()
     }
-    fn find_threasholds(&self, features: &Array2<G>, responses: &Array1<T>, j: usize) -> Array1<G> {
+    fn find_threasholds<T>(
+        &self,
+        features: &Array2<G>,
+        responses: &Array1<T>,
+        j: usize,
+    ) -> Array1<G> {
         // return: a 1-D array with all possible thresholds along feature j
         //(find midpoints between instances of feature j)
         let mut feature_slice = features.select(Axis(1), &[j]);
@@ -147,39 +207,26 @@ where
             Axis(0),
             Vec::from_iter(1..(unique_feature_array.dim())).as_slice(),
         );
-        let mut new = unique_feature_array.add(temp2);
-        let new = new / <G as From<usize>>::from(2);
+        let mut new = unique_feature_array.slice_mut(s![..unique_feature_array.len()-1]) .add(temp2);
+        //dbg!(&new);
+        let new = new / <G as From<u8>>::from(2);
+        //dbg!(&new);
         return new;
     }
-
-    fn train_node(
-        &mut self,
-        node: &mut Node<T, G>,
-        features: &Array2<G>,
-        responses: &Array1<T>,
-        d_try: Option<usize>,
-    ) {
+    pub fn create_correct_node<T: Clone + Into<G> + Into<R> + num::Zero>(&self,features: &Array2<G>,responses: &Array1<T>)->Node<'a,R, G>{
         #[allow(non_snake_case)] // to allow mathematical notation
         let N = features.dim().0;
         #[allow(non_snake_case)]
         let D = features.dim().1;
-        let d_try = d_try.unwrap_or(8);
-
-        let working_node = node;
-
         let mut l_min = <G as From<f64>>::from(INFINITY);
-        let mut j_min = 0;
-        let mut t_min = <G as From<f64>>::from(0_f64);
-        loop {
-            l_min = <G as From<f64>>::from(INFINITY);
-            j_min = 0;
-            t_min = <G as From<f64>>::from(0_f64);
-            let active_indices = RegressionTree::<T, G>::select_active_indices(D, d_try);
+            let mut j_min = 0;
+            let mut t_min = <G as From<f64>>::from(0_f64);
+            let active_indices = RegressionTree::<G, R>::select_active_indices(D, self.d_try);
             for j in active_indices {
                 let threasholds =
-                    RegressionTree::<T, G>::find_threasholds(&self, &features, responses, j);
+                    RegressionTree::<G, R>::find_threasholds(&self, &features, responses, j);
                 for threashold in threasholds {
-                    let loss = RegressionTree::<T, G>::compute_loss_for_split(
+                    let loss = RegressionTree::<G, R>::compute_loss_for_split(
                         self, features, responses, j, threashold,
                     );
                     if loss < l_min {
@@ -191,21 +238,49 @@ where
             }
             if l_min == <G as From<f64>>::from(INFINITY) {
                 // we did not find a threshold, so this should become a leaf node
-                // append leaf node
-                //root
-            }
-        }
+                let sum = responses.sum() ;
+                let mut sum_r: R = sum.into() ;
+                let len_r :R= responses.dim().into();
+                sum_r = sum_r / len_r;
+                return Node::LeafNode::<R, G> { prediction: sum_r };
 
-        todo!()
-    }
-    fn train(&mut self, features: &Array2<G>, responses: &Array1<T>, d_try: Option<usize>) {
+            }
+            else {
+                // threashold exists => split node
+
+                // get left and right
+                let temp_to_split = features.slice(s![.., j_min]);
+                let left_indices = temp_to_split.mapv(|x| (x <= t_min) as usize);
+                let right_indices = left_indices.mapv(|x| 1 - x);
+                let left_feat = features.select(Axis(0), left_indices.as_slice().expect("msg"));
+                let right_feat = features.select(Axis(0), right_indices.as_slice().expect("msg"));
+                let left_resp = responses.select(Axis(0), left_indices.as_slice().expect("msg"));
+                let right_resp = responses.select(Axis(0), right_indices.as_slice().expect("msg"));
+
+                let left = Self::create_correct_node(&self,&left_feat,&left_resp);
+                let right = Self::create_correct_node(&self,&right_feat,&right_resp);
+                return Node::SplitNode::<'a,R, G> {
+                    left: Some(Box::new(left)),
+                    right: Some(Box::new(right)),
+                    split_index: j_min,
+                    threashold: t_min,
+                };
+            }
+        
+        todo!()}
+    pub fn train<T: Clone + Into<G> + Into<R> + num::Zero>(
+        &mut self,
+        features: &Array2<G>,
+        responses: &Array1<T>,
+        d_try: Option<usize>,
+    ) -> Result<String, String> {
         #[allow(non_snake_case)] // to allow mathematical notation
         let N = features.dim().0;
         #[allow(non_snake_case)]
         let D = features.dim().1;
-        let d_try = d_try.unwrap_or(8);
+        let d_try = d_try.unwrap_or(self.d_try);
 
-        let working_node = &mut self.root;
+        //let working_node = &mut self.root;
 
         let mut l_min = <G as From<f64>>::from(INFINITY);
         let mut j_min = 0;
@@ -214,12 +289,12 @@ where
         l_min = <G as From<f64>>::from(INFINITY);
         j_min = 0;
         t_min = <G as From<f64>>::from(0_f64);
-        let active_indices = RegressionTree::<T, G>::select_active_indices(D, d_try);
+        let active_indices = RegressionTree::<G, R>::select_active_indices(D, d_try);
         for j in active_indices {
             let threasholds =
-                RegressionTree::<T, G>::find_threasholds(&self, &features, responses, j);
+                RegressionTree::<G, R>::find_threasholds(&self, &features, responses, j);
             for threashold in threasholds {
-                let loss = RegressionTree::<T, G>::compute_loss_for_split(
+                let loss = RegressionTree::<G, R>::compute_loss_for_split(
                     self, features, responses, j, threashold,
                 );
                 if loss < l_min {
@@ -229,17 +304,85 @@ where
                 }
             }
         }
-        if l_min == <G as From<f64>>::from(INFINITY) {
+        let node = if (l_min == <G as From<f64>>::from(INFINITY)) {
             // we did not find a threshold, so this should become a leaf node
             // append leaf node
             //root
-            let node = Node::LeafNode { prediction: responses.mean() }
-        }
-        // ELSE : assume a split has been found.
-        // Make first Node:
-        let node = Node::SplitNode { left: None, right: None, split_index: j_min, threashold: t_min };
-        self.root = Some(Box::new(node));
+            let sum = responses.sum() ;
+            let mut sum_r: R = sum.into() ;
+            let len_r :R= responses.dim().into();
+            sum_r = sum_r / len_r;
+            Node::LeafNode::<R, G> { prediction: sum_r }
+        } else {
+            // ELSE : assume a split has been found.
+            // Make first Node:
 
+
+            // make left and right
+            let temp_to_split = features.slice(s![.., j_min]);
+            let left_indices = temp_to_split.mapv(|x| (x <= t_min) as usize);
+            let right_indices = left_indices.mapv(|x| 1 - x);
+            let left_feat = features.select(Axis(0), left_indices.as_slice().expect("msg"));
+            let right_feat = features.select(Axis(0), right_indices.as_slice().expect("msg"));
+            let left_resp = responses.select(Axis(0), left_indices.as_slice().expect("msg"));
+            let right_resp = responses.select(Axis(0), right_indices.as_slice().expect("msg"));
+            let left = Self::create_correct_node(&self,&left_feat,&left_resp);
+            let right = Self::create_correct_node(&self,&right_feat,&right_resp);
+
+            Node::SplitNode::<R, G> {
+                left: Some(Box::new(left)),
+                right: Some(Box::new(right)),
+                split_index: j_min,
+                threashold: t_min,
+            }
+        };
+        self.root = Some(Box::new(node));
+        return Ok(String::from("sucess"));
         todo!()
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test(){
+        type G = Float64;
+        type T = i64;
+        type R = Float64;
+        
+        let size = mem::size_of::<Node<R,G>>();
+        println!("Size of MyEnum: {} bytes", size);
+    
+        
+        // let data: Array2<f64> = read_npy("src/digits_data.npy").expect("file is present and correct");
+        // let data = data.mapv(|x| Float64(x));
+        // let target: Array1<i64> =
+        //     read_npy("src/digits_target.npy").expect("file is present and correct");
+        
+        let data: Array2<f64> = read_npy("src/three_9_data.npy").expect("file is present and correct");
+        let data = array![[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]];
+        let data = data.mapv(|x| Float64(x as f64));
+        let target: Array1<i64> =
+        read_npy("src/three_9_target.npy").expect("file is present and correct");
+        let target = array![-1,-1,1,1];
+    
+        let mut tree: RegressionTree<G, R> = RegressionTree {
+            root: None,
+            n_min: 2,
+            d_try:2
+        };
+    
+    
+        tree.train(&data,&target,None);
+        dbg!(&tree);
+        println!("{:?}", data.dim());
+        println!("{:?}", target.dim());
+    
+        let mut results = Vec::new();
+        for row in data.rows() {
+            results.push(tree.predict(&row.to_owned()))
+        }
+        dbg!(&results);
     }
 }
---
+++ src/utils.rs
@@ -0,0 +1,475 @@
+use crate::Float64;
+
+use core::hash::Hash;
+use core::ops::Add;
+use std::cmp::Ordering;
+use std::fmt::Error;
+use std::hash::Hasher;
+use std::ops::{Div, Mul, Neg, Rem, Sub};
+use std::str::FromStr;
+use std::usize;
+//use core::slice::SlicePattern;
+use conv::prelude::*;
+use ndarray::{prelude::*, ScalarOperand, ViewRepr};
+use ndarray_npy::{read_npy, ReadableElement};
+use num::{Float, FromPrimitive, NumCast, One, ToPrimitive, Zero};
+use rand::seq::SliceRandom;
+use std::collections::HashSet;
+use std::{collections::HashMap, hash};
+use std::{f64::INFINITY, marker::PhantomData};
+impl conv::ValueFrom<usize> for Float64 {
+    type Err = Error;
+    fn value_from(src: usize) -> Result<Self, Self::Err> {
+        Ok(Self(src as f64))
+    }
+}
+
+impl From<i32> for Float64 {
+    fn from(value: i32) -> Self {
+        Self(value as f64)
+    }
+}
+
+impl ScalarOperand for Float64 {}
+impl Hash for Float64 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.to_bits().hash(state);
+    }
+}
+
+impl From<usize> for Float64{
+    fn from(value: usize) -> Self {
+        Self(value as f64)
+    }
+}
+
+impl From<i64> for Float64 {
+    fn from(value: i64) -> Self {
+        Self(value as f64)
+    }
+}
+
+impl Add for Float64 {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        Float64(self.0 + other.0)
+    }
+}
+
+impl Sub for Float64 {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        Float64(self.0 - other.0)
+    }
+}
+
+impl Mul for Float64 {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        Float64(self.0 * other.0)
+    }
+}
+
+impl Div for Float64 {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        Float64(self.0 / other.0)
+    }
+}
+
+impl Zero for Float64 {
+    fn zero() -> Self {
+        Float64(0.0)
+    }
+
+    fn is_zero(&self) -> bool {
+        self.0 == 0.0
+    }
+}
+
+impl One for Float64 {
+    fn one() -> Self {
+        Float64(1.0)
+    }
+}
+impl Neg for Float64 {
+    type Output = Float64;
+    fn neg(self) -> Self::Output {
+        Float64(self.0.neg())
+    }
+}
+impl Rem for Float64 {
+    type Output = Float64;
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self(self.0.rem(rhs.0))
+    }
+}
+
+impl FromStr for Float64 {
+    type Err = <f64 as FromStr>::Err;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse::<f64>().map(Float64)
+    }
+}
+
+impl num::Num for Float64 {
+    type FromStrRadixErr = <f64 as num::Num>::FromStrRadixErr;
+
+    fn from_str_radix(str: &str, radix: u32) -> Result<Self, Self::FromStrRadixErr> {
+        f64::from_str_radix(str, radix).map(Float64)
+    }
+}
+impl NumCast for Float64 {
+    fn from<T: ToPrimitive>(n: T) -> Option<Self> {
+        n.to_f64().map(Float64)
+    }
+}
+
+impl Float for Float64 {
+    fn nan() -> Self {
+        Float64(f64::NAN)
+    }
+
+    fn infinity() -> Self {
+        Float64(f64::INFINITY)
+    }
+
+    fn neg_infinity() -> Self {
+        Float64(f64::NEG_INFINITY)
+    }
+
+    fn neg_zero() -> Self {
+        Float64(-0.0)
+    }
+
+    fn min_value() -> Self {
+        Float64(f64::MIN)
+    }
+
+    fn min_positive_value() -> Self {
+        Float64(f64::MIN_POSITIVE)
+    }
+
+    fn epsilon() -> Self {
+        Float64(f64::EPSILON)
+    }
+
+    fn max_value() -> Self {
+        Float64(f64::MAX)
+    }
+
+    fn classify(self) -> std::num::FpCategory {
+        self.0.classify()
+    }
+
+    fn is_nan(self) -> bool {
+        self.0.is_nan()
+    }
+
+    fn is_infinite(self) -> bool {
+        self.0.is_infinite()
+    }
+
+    fn is_finite(self) -> bool {
+        self.0.is_finite()
+    }
+
+    fn is_normal(self) -> bool {
+        self.0.is_normal()
+    }
+
+    fn floor(self) -> Self {
+        Float64(self.0.floor())
+    }
+
+    fn ceil(self) -> Self {
+        Float64(self.0.ceil())
+    }
+
+    fn round(self) -> Self {
+        Float64(self.0.round())
+    }
+
+    fn trunc(self) -> Self {
+        Float64(self.0.trunc())
+    }
+
+    fn fract(self) -> Self {
+        Float64(self.0.fract())
+    }
+
+    fn abs(self) -> Self {
+        Float64(self.0.abs())
+    }
+
+    fn signum(self) -> Self {
+        Float64(self.0.signum())
+    }
+
+    fn is_sign_positive(self) -> bool {
+        self.0.is_sign_positive()
+    }
+
+    fn is_sign_negative(self) -> bool {
+        self.0.is_sign_negative()
+    }
+
+    fn mul_add(self, a: Self, b: Self) -> Self {
+        Float64(self.0.mul_add(a.0, b.0))
+    }
+
+    fn recip(self) -> Self {
+        Float64(self.0.recip())
+    }
+
+    fn powi(self, n: i32) -> Self {
+        Float64(self.0.powi(n))
+    }
+
+    fn powf(self, n: Self) -> Self {
+        Float64(self.0.powf(n.0))
+    }
+
+    fn sqrt(self) -> Self {
+        Float64(self.0.sqrt())
+    }
+
+    fn exp(self) -> Self {
+        Float64(self.0.exp())
+    }
+
+    fn exp2(self) -> Self {
+        Float64(self.0.exp2())
+    }
+
+    fn ln(self) -> Self {
+        Float64(self.0.ln())
+    }
+
+    fn log(self, base: Self) -> Self {
+        Float64(self.0.log(base.0))
+    }
+
+    fn log2(self) -> Self {
+        Float64(self.0.log2())
+    }
+
+    fn log10(self) -> Self {
+        Float64(self.0.log10())
+    }
+
+    fn max(self, other: Self) -> Self {
+        Float64(self.0.max(other.0))
+    }
+
+    fn min(self, other: Self) -> Self {
+        Float64(self.0.min(other.0))
+    }
+
+    fn abs_sub(self, other: Self) -> Self {
+        Float64(self.0.abs_sub(other.0))
+    }
+
+    fn cbrt(self) -> Self {
+        Float64(self.0.cbrt())
+    }
+
+    fn hypot(self, other: Self) -> Self {
+        Float64(self.0.hypot(other.0))
+    }
+
+    fn sin(self) -> Self {
+        Float64(self.0.sin())
+    }
+
+    fn cos(self) -> Self {
+        Float64(self.0.cos())
+    }
+
+    fn tan(self) -> Self {
+        Float64(self.0.tan())
+    }
+
+    fn asin(self) -> Self {
+        Float64(self.0.asin())
+    }
+
+    fn acos(self) -> Self {
+        Float64(self.0.acos())
+    }
+
+    fn atan(self) -> Self {
+        Float64(self.0.atan())
+    }
+
+    fn atan2(self, other: Self) -> Self {
+        Float64(self.0.atan2(other.0))
+    }
+
+    fn sin_cos(self) -> (Self, Self) {
+        let (sin, cos) = self.0.sin_cos();
+        (Float64(sin), Float64(cos))
+    }
+
+    fn exp_m1(self) -> Self {
+        Float64(self.0.exp_m1())
+    }
+
+    fn ln_1p(self) -> Self {
+        Float64(self.0.ln_1p())
+    }
+
+    fn sinh(self) -> Self {
+        Float64(self.0.sinh())
+    }
+
+    fn cosh(self) -> Self {
+        Float64(self.0.cosh())
+    }
+
+    fn tanh(self) -> Self {
+        Float64(self.0.tanh())
+    }
+
+    fn asinh(self) -> Self {
+        Float64(self.0.asinh())
+    }
+
+    fn acosh(self) -> Self {
+        Float64(self.0.acosh())
+    }
+
+    fn atanh(self) -> Self {
+        Float64(self.0.atanh())
+    }
+
+    fn integer_decode(self) -> (u64, i16, i8) {
+        self.0.integer_decode()
+    }
+
+    fn to_degrees(self) -> Self {
+        Float64(self.0.to_degrees())
+    }
+
+    fn to_radians(self) -> Self {
+        Float64(self.0.to_radians())
+    }
+}
+
+impl Eq for Float64 {}
+impl Ord for Float64 {
+    fn cmp(&self, other: &Self) -> Ordering {
+        // Define a custom ordering for NaN values
+        if self.0.is_nan() && other.0.is_nan() {
+            Ordering::Equal
+        } else if self.0.is_nan() {
+            Ordering::Less
+        } else if other.0.is_nan() {
+            Ordering::Greater
+        } else {
+            self.0.partial_cmp(&other.0).unwrap()
+        }
+    }
+}
+impl From<f64> for Float64 {
+    fn from(value: f64) -> Self {
+        Self(value)
+    }
+}
+impl FromPrimitive for Float64 {
+    fn from_i64(n: i64) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_u64(n: u64) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_f64(n: f64) -> Option<Self> {
+        Some(Float64(n))
+    }
+
+    fn from_isize(n: isize) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_i8(n: i8) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_i16(n: i16) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_i32(n: i32) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_usize(n: usize) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_u8(n: u8) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_u16(n: u16) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+
+    fn from_u32(n: u32) -> Option<Self> {
+        Some(Float64(n as f64))
+    }
+}
+
+impl ToPrimitive for Float64 {
+    fn to_i64(&self) -> Option<i64> {
+        Some(self.0 as i64)
+    }
+
+    fn to_u64(&self) -> Option<u64> {
+        Some(self.0 as u64)
+    }
+
+    fn to_f64(&self) -> Option<f64> {
+        Some(self.0)
+    }
+
+    fn to_isize(&self) -> Option<isize> {
+        Some(self.0 as isize)
+    }
+
+    fn to_i8(&self) -> Option<i8> {
+        Some(self.0 as i8)
+    }
+
+    fn to_i16(&self) -> Option<i16> {
+        Some(self.0 as i16)
+    }
+
+    fn to_i32(&self) -> Option<i32> {
+        Some(self.0 as i32)
+    }
+
+    fn to_usize(&self) -> Option<usize> {
+        Some(self.0 as usize)
+    }
+
+    fn to_u8(&self) -> Option<u8> {
+        Some(self.0 as u8)
+    }
+
+    fn to_u16(&self) -> Option<u16> {
+        Some(self.0 as u16)
+    }
+
+    fn to_u32(&self) -> Option<u32> {
+        Some(self.0 as u32)
+    }
+}
+impl From<u8> for Float64 {
+    fn from(value: u8) -> Self {
+        Self(value as f64)
+    }
+}
